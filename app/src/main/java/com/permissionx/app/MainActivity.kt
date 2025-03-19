8a30aba96cebf73d57a94de92fbceda95b2e07a8
Tony <tony@gmail.com>
2025-03-19 17:57:58 +0800

    完成PermissionX库的开发与测试工作。

---
+++ app/src/main/java/com/permissionx/app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.permissionx.app
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.permissionx.app.databinding.ActivityMainBinding
+import com.permissionx.huang.PermissionX
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        binding?.makeCallBtn?.setOnClickListener {
+            PermissionX.request(this,
+            android.Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+}
